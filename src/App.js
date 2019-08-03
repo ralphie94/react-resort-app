2a8e96dfa869168e1450b9a6dd73c72e9abffb7f
Ralphie Celedon <ralphie@ralphies-mbp.attlocal.net>
2019-08-03 00:00:32 -0700

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>hello from app</div>
+    <div>
+      <Route path="/" component={Home}/>
+      <Route path="/rooms" component={Rooms}/>
+      <Route path="/single-room" component={SingleRoom}/>
+    </div>
   );
 }
 
