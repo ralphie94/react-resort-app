9c3ca1006aabb8267ad8311fcc77fe5d611d17ab
Ralphie Celedon <ralphie@ralphies-mbp.attlocal.net>
2019-08-03 00:10:40 -0700

    switch setup, created navbar

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <Route path="/" component={Home}/>
-      <Route path="/rooms" component={Rooms}/>
-      <Route path="/single-room" component={SingleRoom}/>
+      <Navbar />
+      <Switch>
+       <Route exact path="/" component={Home} />
+       <Route exact path="/rooms" component={Rooms} />
+       <Route exact path="/rooms/:moo" component={SingleRoom} />
+       <Route component={Error} />
+      </Switch>
     </div>
   );
 }
